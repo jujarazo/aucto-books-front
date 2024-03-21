e08d94eaf208541005053d833ae45641f180faf1
JuJarazo <julianjarazo94@gmail.com>
2024-03-20 21:33:55 -0300

    Home grid responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Book } from '@/components/home/book';
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full">
       <h1 className="text-4xl font-bold mb-4">Current books</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Book />
--- src/components/home/book.tsx
+++ src/components/home/book.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -9,8 +8,6 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 
 export function Book() {
   return (
